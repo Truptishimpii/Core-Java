5bdbeb62cd06547d3a8950b2ce2b5bf3848c1fcf
Truptishimpii <163382610+Truptishimpii@users.noreply.github.com>
2024-08-20 07:58:31 +0530

    Create Introduction.java

---
+++ Introduction.java
@@ -0,0 +1,13 @@
+package lecture1;
+
+public class basic {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World !!");
+		System.out.println("the sum is: " + 10 + (10+2));
+		
+
+	}
+
+}
