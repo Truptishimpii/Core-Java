728d9640712301e58c6a6434d11feb5f9994efd9
Truptishimpii <163382610+Truptishimpii@users.noreply.github.com>
2024-09-19 08:03:03 +0530

    Create polymorphism_constructor.java

---
+++ polymorphism_constructor.java
@@ -0,0 +1,28 @@
+package method;
+// polymorphism 
+public class method_overloading {
+	// through constructor 
+	
+	void info() {
+		System.out.println("Hello Users !");
+	}
+	
+	void info(String Name) {
+		System.out.println("Name: "+Name);
+	}
+	
+	void info(int age, float height) {
+		System.out.println("Age : "+age);
+		System.out.println("height : "+height);
+	}
+	
+	public static void main(String[] args) {
+		method_overloading method = new method_overloading();
+		// method(name created) is object 
+	// calling contructor through object & overloading through datatypes.
+		method.info();
+		method.info("Trupti");
+		method.info(20,5.3f);
+		
+	}
+}
